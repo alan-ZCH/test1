aa4e9b7a3cff232e2a43bf9aa368be7862b994b4
alan-ZCH <1136559229@qq.com>
2019-05-16 22:31:44 +0800

    haha

--- src/MyTest.java
+++ src/MyTest.java
@@ -2,6 +2,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("my");
-        System.out.println("test2");
+        System.out.println("test1");
     }
 }
